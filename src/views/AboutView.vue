e7811fed3b11b3cf03172f7f9c34a0c8e91749b9
Kamil Kałuża <kamil321.94@gmail.com>
2023-02-11 15:05:35 +0100

    init

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic eaque numquam saepe tenetur aliquid, eum doloribus impedit commodi corrupti fugiat ut! Tempore, magnam. Blanditiis praesentium, ipsa dolor vero voluptate quidem!</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic eaque numquam saepe tenetur aliquid, eum doloribus impedit commodi corrupti fugiat ut! Tempore, magnam. Blanditiis praesentium, ipsa dolor vero voluptate quidem!</p>
   </div>
 </template>
